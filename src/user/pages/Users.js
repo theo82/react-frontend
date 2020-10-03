09c9148bfc3e326cc289c5d5621f8141d2961918
theo82 <theoaristi53@gmail.com>
2020-10-03 11:44:37 +0300

    Adding a UserList Page/Component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Theo',
+      image:
+        'https://texashillcountry.com/wp-content/uploads/harveycat-660x400.jpg',
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
