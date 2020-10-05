3c8bf13f3d4e9dec78b1477655d9c6b16e13df10
theo82 <theoaristi53@gmail.com>
2020-10-05 11:28:32 +0300

    Handling the Drawer State

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MainNavigation.css';
 import MainHeader from './MainHeader';
 import { Link } from 'react-router-dom';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
 const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
     <React.Fragment>
-      <SideDrawer>
-        <nav className='main-navigation__drawer-nav'>
-          <NavLinks />
-        </nav>
-      </SideDrawer>
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className='main-navigation__drawer-nav'>
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
       <MainHeader>
-        <button className='main-navigation__menu-btn'>
+        <button className='main-navigation__menu-btn' onClick={openDrawer}>
           <span></span>
           <span></span>
           <span></span>
