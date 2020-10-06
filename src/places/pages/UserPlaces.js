3b0d6c354ee478ea7509ddc6bf8e69efb663f10f
theo82 <theoaristi53@gmail.com>
2020-10-06 11:20:49 +0300

    Getting Route Params

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PlaceList from '../components/PlaceList';
-
+import { useParams } from 'react-router-dom';
 const DUMMY_PLACES = [
   {
     id: 'p1',
@@ -30,7 +30,9 @@ const DUMMY_PLACES = [
   },
 ];
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
