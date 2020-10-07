d0bd53322f4ef719272f57506ef704cadcd59ff7
theo82 <theoaristi53@gmail.com>
2020-10-07 12:36:21 +0300

    Add a Custom Form Input Component

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -13,7 +13,7 @@ const NavLinks = (props) => {
         <NavLink to='/u1/places'>MY PLACES</NavLink>
       </li>
       <li>
-        <NavLink to='/places/new'>NEW PLACE</NavLink>
+        <NavLink to='/places/new'>ADD PLACE</NavLink>
       </li>
       <li>
         <NavLink to='/auth'>AUTHENTICATE</NavLink>
