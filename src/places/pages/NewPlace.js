5045775e9c7f30e087fcceda4d014ae44ec425fd
theo82 <theoaristi53@gmail.com>
2020-10-10 09:54:07 +0300

    Finishing the Add Place Form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -57,8 +57,13 @@ const NewPlace = () => {
     });
   }, []);
 
+  const placeSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
   return (
-    <form className='place-form'>
+    <form className='place-form' onSubmit={placeSubmitHandler}>
       <Input
         id='title'
         element='input'
@@ -74,6 +79,14 @@ const NewPlace = () => {
         label='Description'
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText='Please enter a valid description (at least 5 characters).'
+        onInput={inputHandler}
+      />
+      <Input
+        id='address'
+        element='input'
+        label='Address'
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText='Please enter a valid address'
         onInput={inputHandler}
       />
       <Button type='submit' disabled={!formState.isValid}>
