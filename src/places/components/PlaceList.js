dbb40489fa6ec9f15361a2a635e0d1d8613edfe6
theo82 <theoaristi53@gmail.com>
2020-10-12 18:05:33 +0300

    Fixing Minor Issures

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import PlaceItem from './PlaceItem';
-
+import Button from '../../shared/components/FormElements/Button';
 import './PlaceList';
 
 const PlaceList = (props) => {
@@ -11,7 +11,7 @@ const PlaceList = (props) => {
       <div className='place-list center'>
         <Card>
           <h2>No places found. Maybe create one?</h2>
-          <button>Shared Place</button>
+          <Button to='/places/new'>Shared Place</Button>
         </Card>
       </div>
     );
--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,13 +1,15 @@
-import React, { useEffec, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
+import Card from '../../shared/components/UIElements/Card';
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH,
 } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
+import './PlaceForm.css';
 
 const DUMMY_PLACES = [
   {
@@ -25,7 +27,7 @@ const DUMMY_PLACES = [
   },
   {
     id: 'p2',
-    title: 'Empire State Building',
+    title: 'Emp. State Building',
     description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
@@ -39,39 +41,42 @@ const DUMMY_PLACES = [
 ];
 
 const UpdatePlace = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const placeId = useParams().placeId;
 
   const [formState, inputHandler, setFormData] = useForm(
     {
       title: {
-        value: identifiedPlace.title,
+        value: '',
         isValid: false,
       },
       description: {
-        value: identifiedPlace.description,
+        value: '',
         isValid: false,
       },
     },
-    true
+    false
   );
 
   const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
   useEffect(() => {
-    setFormData(
-      {
-        title: {
-          value: identifiedPlace.title,
-          isValid: true,
+    if (identifiedPlace) {
+      setFormData(
+        {
+          title: {
+            value: identifiedPlace.title,
+            isValid: true,
+          },
+          description: {
+            value: identifiedPlace.description,
+            isValid: true,
+          },
         },
-        description: {
-          value: identifiedPlace.description,
-          isValid: true,
-        },
-      },
-      true
-    );
+        true
+      );
+    }
+
     setIsLoading(false);
   }, [setFormData, identifiedPlace]);
 
@@ -79,10 +84,13 @@ const UpdatePlace = () => {
     event.preventDefault();
     console.log(formState.inputs);
   };
+
   if (!identifiedPlace) {
     return (
       <div className='center'>
-        <h2>Could not find place!</h2>
+        <Card>
+          <h2>Could not find place!</h2>
+        </Card>
       </div>
     );
   }
@@ -114,8 +122,7 @@ const UpdatePlace = () => {
         label='Description'
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText='Please enter a valid description (min. 5 characters).'
-        onInput={() => {}}
-        value={inputHandler}
+        onInput={inputHandler}
         initialValue={formState.inputs.description.value}
         initialValid={formState.inputs.description.isValid}
       />
