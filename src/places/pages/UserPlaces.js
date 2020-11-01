60dbc79cef31ea323764eb0a5311f12cbccdef62
theo82 <theoaristi53@gmail.com>
2020-11-01 13:52:14 +0200

    Final Adjustments

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import PlaceList from '../components/PlaceList';
 import { useParams } from 'react-router-dom';
+
+import PlaceList from '../components/PlaceList';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
-
-import { httpClientHook, useHttpClient} from '../../shared/hooks/http-hook';
-
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 const UserPlaces = () => {
   const [loadedPlaces, setLoadedPlaces] = useState();
@@ -15,28 +14,35 @@ const UserPlaces = () => {
 
   useEffect(() => {
     const fetchPlaces = async () => {
-        try {
-          const responseData = await sendRequest(
-            `http://localhost:5000/api/places/user/${userId}`
-          );
-          setLoadedPlaces(responseData.places);
-        }catch(err) {}
-      }
-      fetchPlaces();
-  }, [sendRequest, userId])
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/places/user/${userId}`
+        );
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
+    };
+    fetchPlaces();
+  }, [sendRequest, userId]);
 
-  return(
+  const placeDeletedHandler = deletedPlaceId => {
+    setLoadedPlaces(prevPlaces =>
+      prevPlaces.filter(place => place.id !== deletedPlaceId)
+    );
+  };
+
+  return (
     <React.Fragment>
-      <ErrorModal error={error} onClear={clearError}/>
+      <ErrorModal error={error} onClear={clearError} />
       {isLoading && (
         <div className="center">
           <LoadingSpinner />
         </div>
       )}
-      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />};
+      {!isLoading && loadedPlaces && (
+        <PlaceList items={loadedPlaces} onDeletePlace={placeDeletedHandler} />
+      )}
     </React.Fragment>
-  )
-
+  );
 };
 
 export default UserPlaces;
