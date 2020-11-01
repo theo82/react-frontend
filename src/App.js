20887b24ae64543c61287ac5c796d795b33460b5
theo82 <theoaristi53@gmail.com>
2020-11-01 12:21:07 +0200

    Fixing nav links and adding delete place funtionality

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, {useCallback, useState} from 'react';
+import React, { useState, useCallback } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
-  Switch,
+  Switch
 } from 'react-router-dom';
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
 import Auth from './user/pages/Auth';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userId, setUserId] = useState(false);
 
-  const login = useCallback((uid) => {
+  const login = useCallback(uid => {
     setIsLoggedIn(true);
     setUserId(uid);
-  }, [])
+  }, []);
 
-  const logout = useCallback((uid) => {
-    setIsLoggedIn(false)
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
     setUserId(null);
-  },[])
+  }, []);
 
   let routes;
 
@@ -66,21 +66,18 @@ const App = () => {
   }
 
   return (
-    <AuthContext.Provider value=
-    {{
-      isLoggedIn: isLoggedIn, 
-      userId: userId, 
-      login: login, 
-      logout: logout
-    }}>
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          {routes}
-        </Switch>
-      </main>
-    </Router>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        userId: userId,
+        login: login,
+        logout: logout
+      }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>{routes}</main>
+      </Router>
     </AuthContext.Provider>
   );
 };
