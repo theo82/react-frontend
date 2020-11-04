8362cd23e0442d40fd9fcf85afcbd2d99c07f932
theo82 <theoaristi53@gmail.com>
2020-11-04 11:42:11 +0200

    Using & Attaching JWT (TOKEN) in React

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -51,12 +51,9 @@ const NewPlace = () => {
       formData.append('address', formState.inputs.address.value);
       formData.append('creator', auth.userId);
       formData.append('image', formState.inputs.image.value);
-
-      await sendRequest(
-        'http://localhost:5000/api/places',
-        'POST',
-        formData
-      );
+      await sendRequest('http://localhost:5000/api/places', 'POST', formData, {
+        Authorization: 'Bearer ' + auth.token
+      });
       history.push('/');
     } catch (err) {}
   };
@@ -91,10 +88,10 @@ const NewPlace = () => {
           errorText="Please enter a valid address."
           onInput={inputHandler}
         />
-        <ImageUpload 
-            id="image" 
-            onInput={inputHandler} 
-            errorText="Please provide an image"
+        <ImageUpload
+          id="image"
+          onInput={inputHandler}
+          errorText="Please provide an image."
         />
         <Button type="submit" disabled={!formState.isValid}>
           ADD PLACE
