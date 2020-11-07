22cb1e186fe2d98b440aeaa95271b6f0e5778da2
theo82 <theoaristi53@gmail.com>
2020-11-07 07:41:35 +0200

    Environment Variables in React Apps

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -13,7 +13,7 @@ const Users = () => {
     const sendRequest = async () => {
       setIsLoading(true);
       try {
-        const response = await fetch('http://localhost:5000/api/users');
+        const response = await fetch(process.env.REACT_APP_BACKEND_URL + '/users');
 
         const responseData = await response.json();
 
